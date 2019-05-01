ee1cca7078627c44b17deec03b8acfc8a289a6e5
Yuridia Larios Aispuro <yuriydani@gmail.com>
2019-05-01 11:05:00 -0700

    callin api folder

---
+++ 03-Calling-an-API/src/Auth/auth0-variables.js
@@ -0,0 +1,8 @@
+export const AUTH_CONFIG = {
+  domain: 'princess-minina.auth0.com',
+  clientId: '6oOd0JH6BQ7VD5YPMJkrzimfpgII6T9X',
+  redirectUri: 'http://localhost:3000/callback',
+  // apiUrl: 'http://localhost:3001',
+  audience: 'http://localhost:3001',
+}
+
